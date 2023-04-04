71592eee0bb82b1159e67e2655b874d678acdcda
YukKaji <dedyshka291@gmail.com>
2023-04-04 23:47:09 +0700

    Another on 2

--- jscript.js
+++ jscript.js
@@ -1,4 +1,7 @@
  'use strict';
+
+ //smeeth movement
+
  $(document).ready(function(){
     $("#head").on("click","a", function (event) {
         event.preventDefault();
@@ -7,3 +10,50 @@
         $('body,html').animate({scrollTop: top}, 800);
     });
 });
+
+// corousel
+
+const track = document.querySelector('.carousel-track');
+const slides = Array.from(track.children);
+const prevButton = document.querySelector('.carousel-button-prev');
+const nextButton = document.querySelector('.carousel-button-next');
+const slideWidth = slides[0].getBoundingClientRect().width;
+const maxSlides = 1;
+let currentSlide = 0;
+
+track.style.transform = `translateX(-${slideWidth * currentSlide}px)`;
+
+function moveSlides(direction) {
+    if (direction === 'prev') {
+        currentSlide--;
+    } else {
+        currentSlide++;
+    }
+  
+    track.style.transform = `translateX(-${slideWidth * currentSlide}px)`;
+    updateButtons();
+}
+
+function updateButtons() {
+    if (currentSlide === 0) {
+        prevButton.disabled = true;
+    } else {
+        prevButton.disabled = false;
+    }
+  
+    if (currentSlide === slides.length - maxSlides) {
+        nextButton.disabled = true;
+    } else {
+        nextButton.disabled = false;
+    }
+}
+
+prevButton.addEventListener('click', () => {
+    moveSlides('prev');
+});
+
+nextButton.addEventListener('click', () => {
+    moveSlides('next');
+});
+
+updateButtons();
