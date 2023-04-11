e9326c8473f9b37ec361cfc23fb9c124c1fb39cd
YukKaji <dedyshka291@gmail.com>
2023-04-11 20:18:46 +0700

    oooooooOOOOO

--- jscript.js
+++ jscript.js
@@ -18,53 +18,6 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
-// corousel
-
-const track = document.querySelector('.carousel-track');
-const slides = Array.from(track.children);
-const prevButton = document.querySelector('.carousel-button-prev');
-const nextButton = document.querySelector('.carousel-button-next');
-const slideWidth = slides[0].getBoundingClientRect().width;
-const maxSlides = 1;
-let currentSlide = 0;
-
-track.style.transform = `translateX(-${slideWidth * currentSlide}px)`;
-
-function moveSlides(direction) {
-    if (direction === 'prev') {
-        currentSlide--;
-    } else {
-        currentSlide++;
-    }
-  
-    track.style.transform = `translateX(-${slideWidth * currentSlide}px)`;
-    updateButtons();
-}
-
-function updateButtons() {
-    if (currentSlide === 0) {
-        prevButton.disabled = true;
-    } else {
-        prevButton.disabled = false;
-    }
-  
-    if (currentSlide === slides.length - maxSlides) {
-        nextButton.disabled = true;
-    } else {
-        nextButton.disabled = false;
-    }
-}
-
-prevButton.addEventListener('click', () => {
-    moveSlides('prev');
-});
-
-nextButton.addEventListener('click', () => {
-    moveSlides('next');
-});
-
-updateButtons();
-
 // lines
 
 window.addEventListener('scroll', function() {
@@ -107,12 +60,16 @@ window.addEventListener('scroll', function() {
     }
 });
 
-// animation for pics in portfolio
+// pics in portfolio
 
-let img = document.querySelector('#port_meme_img');
-let opacity = 0;
-let interval = setInterval(function() {
-  opacity += 0.01;
-  img.style.opacity = opacity;
-  if (opacity >= 1) clearInterval(interval);
-}, 10);
+function zoomIn(img) {
+    let zoomed = document.createElement("div");
+    zoomed.className = "zoomed";
+    let zoomedImg = document.createElement("img");
+    zoomedImg.src = img.src;
+    zoomedImg.onclick = function() {
+        document.body.removeChild(zoomed);
+    }
+    zoomed.appendChild(zoomedImg);
+    document.body.appendChild(zoomed);
+}
